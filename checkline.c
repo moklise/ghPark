def84584a0142893f30e3725b22c1875f37e94c2
moklise <moklise@gmail.com>
2015-03-29 06:01:42 +0900

    단어빈도검색 기능추가

--- checkline.c
+++ checkline.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+int findChar(char *buffer);
+
 int main(void){
 	int NumberOfLine=0;
 	FILE *fp;
 	char fname[30];
 	char buffer[100];
+	int numberChar=0;
 
 	printf( "파일이름을 입력해주세요 :" );
 	scanf( "%s", fname );	
@@ -17,7 +20,19 @@ int main(void){
 
 	while(fgets(buffer, 99, fp)){
 		NumberOfLine++;
+
+		numberChar += findChar(buffer);
+
 	}
 
-	printf( "총 라인의 개수는 %d 개 입니다.", NumberOfLine );
+	printf( "총 라인의 개수는 %d 개 입니다.\n", NumberOfLine );
+	printf( "줄 이라는 단어의 빈도는 %d 번 입니다.\n", numberChar );
+}
+
+int findChar(char *buffer){
+	if(strstr(buffer, "줄")){
+		return 1;
+	}else{
+		return 0;
+	}
 }
